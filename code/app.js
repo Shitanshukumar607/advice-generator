f43deba418c1a4e0f20a2eeeafdafc4d91f9db68
Shitanshukumar607 <147914822+Shitanshukumar607@users.noreply.github.com>
2024-06-24 09:10:03 +0530

    fixed the error on fetching api

--- code/app.js
+++ code/app.js
@@ -2,18 +2,16 @@ const getData = async (num) => {
     try{
         const response = await fetch(`https://api.adviceslip.com/advice/${num}`)
         const data = await response.json()
-        console.log(data);
         editDOM(data)
     }
     catch (error){
         console.log(error);
-        alert('There was an error fetching the data try again lol.')
+        document.querySelector(".advice").textContent = `Couldnt get data from the API try again :(`
     }
 }
 
 function getAdvice(){
     let x = Math.floor(Math.random() * 200)
-    console.log(x);
     getData(x)
 }
 
